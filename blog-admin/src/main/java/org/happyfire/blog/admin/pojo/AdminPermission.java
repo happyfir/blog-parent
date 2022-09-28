ad05cb05d166b62cb1a781807d73164ac4adfd8d
happyfir <39881443+happyfir@users.noreply.github.com>
2022-09-29 00:24:12 +0800

    对后台用户进行管理

---
+++ blog-admin/src/main/java/org/happyfire/blog/admin/pojo/AdminPermission.java
@@ -0,0 +1,10 @@
+package org.happyfire.blog.admin.pojo;
+
+import lombok.Data;
+
+@Data
+public class AdminPerssion {
+    private Long id;
+    private Long adminId;
+    private Long permissionId;
+}
