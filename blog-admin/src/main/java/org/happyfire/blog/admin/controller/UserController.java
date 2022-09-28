73574303a0ed3a739359aaf337ff6324087a56f2
happyfir <39881443+happyfir@users.noreply.github.com>
2022-09-28 11:15:11 +0800

    对后台系统代码进行规范化

---
+++ blog-admin/src/main/java/org/happyfire/blog/admin/controller/UserController.java
@@ -0,0 +1,35 @@
+package org.happyfire.blog.admin.controller;
+
+import org.happyfire.blog.admin.model.params.PageParam;
+import org.happyfire.blog.admin.pojo.Permission;
+import org.happyfire.blog.admin.service.PermissionService;
+import org.happyfire.blog.admin.vo.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("permission")
+public class PermissionController {
+    @Autowired
+    private PermissionService permissionService;
+
+    @PostMapping("permissionList")
+    public Result listPermission(@RequestBody PageParam pageParam) {
+        return permissionService.listPermission(pageParam);
+    }
+
+    @PostMapping("add")
+    public Result add(@RequestBody Permission permission) {
+        return permissionService.add(permission);
+    }
+
+    @PostMapping("update")
+    public Result update(@RequestBody Permission permission) {
+        return permissionService.update(permission);
+    }
+
+    @GetMapping("delete/{id}")
+    public Result delete(@PathVariable("id") Long id) {
+        return permissionService.delete(id);
+    }
+}
